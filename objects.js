e45fcac4a85cc2f546b80f38fd09d8bb9f62b12b
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-02-10 18:55:37 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
-var playlist = { Alison: "Slowdive", Sometimes: 'My Bloody Valentine'}
+var playlist = { Slowdive: "Alison", Sometimes: 'My Bloody Valentine'}
 function updatePlaylist(playlist, artistName, songTitle) { playlist[artistName]= songTitle
 return playlist }
-function removeFromPlaylist(playlist, artistName) {delete playlist.Alison 
+function removeFromPlaylist(playlist, artistName) {delete playlist.Slowdive 
 return playlist}
