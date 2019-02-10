cb4ca59bce83832b05a84df0a9489cebc863c88f
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-02-10 18:29:23 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,3 +1,3 @@
 var playlist = { Alison: "Slowdive"}
-function updatePlaylist(playlist, artistName, songTitle) { playlist[Sometimes]= "My Bloody Valentine"
+function updatePlaylist(playlist, artistName, songTitle) { playlist[artisyName]= songTitle
 return playlist }
